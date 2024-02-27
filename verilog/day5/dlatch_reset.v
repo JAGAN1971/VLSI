99538b8a157d739ef3feaee2a129370e217815ae
JAGAN1971 <155874326+JAGAN1971@users.noreply.github.com>
2024-02-27 10:35:05 +0530

    Create dlatch_reset.v

---
+++ verilog/day5/dlatch_reset.v
@@ -0,0 +1,12 @@
+module dlatch_reset(data, en, reset, q);
+  input data, en, reset;
+  output q;
+  reg q;
+  
+  always @ (data or en or reset)
+    if(reset) begin
+      q<=1'b0;
+    end else if (en) begin
+      q<=data;
+    end
+endmodule
