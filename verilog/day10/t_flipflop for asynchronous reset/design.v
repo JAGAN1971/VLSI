f7ade93a3e0d821fb78fde9d495e979709f4e555
JAGAN1971 <155874326+JAGAN1971@users.noreply.github.com>
2024-03-05 00:21:37 +0530

    Create design.v

---
+++ verilog/day10/t_flipflop for asynchronous reset/design.v
@@ -0,0 +1,17 @@
+module T_flipflop (
+  input clk, rst_n,
+  input t,
+  output reg q,
+  output q_bar
+  );
+  
+ always@(posedge clk or negedge rst_n) // for asynchronous reset
+
+    if(!rst_n) q <= 0;
+    else begin
+      q <= (t?~q:q);
+    end
+  end
+  assign q_bar = ~q;
+endmodule
+
